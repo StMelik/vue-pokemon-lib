314a6f82d903f083eaf92ab9288a2ee08b3173ae
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-10 01:23:30 +0300

    feat: верстка хедера

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,54 @@
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <MainHeader />
+    <h1>Hello</h1>
+  </div>
 </template>
 
+<script>
+import MainHeader from './Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader
+  }
+}
+</script>
+
 <style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 body {
   color: #E5E5E5;
   background-color: #222222;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+  transition: opacity .3s;
+}
+
+button {
+  border: none;
+  color: inherit;
+  background-color: transparent;
+  cursor: pointer;
+  transition: opacity .3s;
+}
+
+a:hover,
+button:hover {
+  opacity: .5;
 }
 
 #app {
@@ -13,5 +56,11 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+
+.container {
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 0 15px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+    <header class="header">
+        <nav class="header__nav nav-header">
+            <ul class="nav-header__list">
+                <li>
+                    <router-link class="nav-header__link" to="/">Home</router-link>
+                </li>
+            </ul>
+        </nav>
+
+        <form class="header__search search-header">
+            <input class="search-header__input" type="text" placeholder="Search...">
+            <button class="search-header__button"></button>
+        </form>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'main-header'
+}
+</script>
+
+<style>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    column-gap: 50px;
+    padding: 30px;
+    border-bottom: 1px solid #E5E5E5;
+}
+
+.nav-header__list {
+    display: flex;
+    column-gap: 35px;
+}
+
+.nav-header__link {
+    font-size: 30px;
+}
+
+.search-header {
+    display: flex;
+}
+
+.search-header__input {
+    padding: 5px 15px;
+    border: 2px solid #E5E5E5;
+    border-radius: 20px 0 0 20px;
+    background-color: transparent;
+    color: inherit;
+    font-size: 26px;
+    max-width: 250px;
+    height: 40px;
+}
+
+.search-header__button {
+    width: 50px;
+    height: 40px;
+    border-radius: 0 20px 20px 0;
+    background-color: #E5E5E5;
+    background-image: url('../assets/icons/search-icon.svg');
+    background-repeat: no-repeat;
+    background-position: 40% center;
+    background-size: 25px 25px;
+}
+</style>
