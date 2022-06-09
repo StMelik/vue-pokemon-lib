50c4b44f8feda806339ccf80ac98dd8f10f8b3b1
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-10 00:52:43 +0300

    init: init

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style>
+body {
+  color: #E5E5E5;
+  background-color: #222222;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
